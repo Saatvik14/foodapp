0716d7d3f0c741431c20d23c18e098f55465de4f
unknown <saatvikrawat8921@gmail.com>
2020-09-13 00:21:37 +0530

    Updated Signup page and added main page of App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,128 +15,317 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-            body: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(40.0),
-              child: Center(
-                child: Container(
-                  child: Image(image: AssetImage('images/foodlogo.png')),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10),
-              child: Text(
-                'Sign Up free account',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
+          body: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
               child: Column(
                 children: <Widget>[
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      hintText: 'UserName',
-                      fillColor: Colors.grey[800],
-                      hintStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.grey[600],
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(60.0),
+                  Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Center(
+                      child: Container(
+                        child: Image(image: AssetImage('images/foodlogo.png')),
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 20,
                   ),
-                  TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      fillColor: Colors.grey[800],
-                      hintText: 'Email',
-                      hintStyle: TextStyle(
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(
+                      'Sign Up free account',
+                      style: TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.grey[600],
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(60.0),
+                        fontSize: 25,
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 20,
                   ),
-                  TextField(
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      fillColor: Colors.grey[800],
-                      hintStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.grey[600],
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(60.0),
-                      ),
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        TextField(
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                            hintText: 'UserName',
+                            fillColor: Colors.grey[800],
+                            hintStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.grey[600],
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(60.0),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey[800],
+                            hintText: 'Email',
+                            hintStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.grey[600],
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(60.0),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.name,
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            fillColor: Colors.grey[800],
+                            hintStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.grey[600],
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(60.0),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            hintText: 'Phone',
+                            fillColor: Colors.grey[800],
+                            hintStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.grey[600],
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(60.0),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
+                    margin: EdgeInsets.all(10),
                   ),
                   SizedBox(
-                    height: 10,
+                    height: 15,
                   ),
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      hintText: 'Phone',
-                      fillColor: Colors.grey[800],
-                      hintStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.grey[600],
+                  Center(
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(40)),
+                      color: Colors.blue,
+                      child: Text(
+                        'Submit',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(60.0),
-                      ),
+                      textColor: Colors.white,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => App()),
+                        );
+                      },
                     ),
-                  ),
+                  )
                 ],
-              ),
-              margin: EdgeInsets.all(10),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Center(
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40)),
-                color: Colors.blue,
-                child: Text(
-                  'Submit',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
+              )),
+        ),
+      ),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  String cir({String c}) {
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height / 4.5,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.blue[400],
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 235),
+                      child: Image(
+                        image: AssetImage('images/foodlogo.png'),
+                        height: 50,
+                        width: 100,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: CircleAvatar(
+                                  radius: 25,
+                                  backgroundColor: Colors.white,
+                                  child: Image(
+                                    image: AssetImage('images/burger.jpg'),
+                                    width: 35,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20),
+                                child: Text(
+                                  'Burger',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 28,
+                          ),
+                          Column(
+                            children: <Widget>[
+                              CircleAvatar(
+                                radius: 25,
+                                backgroundColor: Colors.white,
+                                child: Image(
+                                  image: AssetImage('images/pizza.jpg'),
+                                  width: 28,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                'Pizza',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 28,
+                          ),
+                          Column(
+                            children: <Widget>[
+                              CircleAvatar(
+                                radius: 25,
+                                backgroundColor: Colors.white,
+                                child: Image(
+                                  image: AssetImage('images/drink.jpg'),
+                                  width: 28,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                'drink',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 28,
+                          ),
+                          Column(
+                            children: <Widget>[
+                              CircleAvatar(
+                                radius: 25,
+                                backgroundColor: Colors.white,
+                                child: Image(
+                                  image: AssetImage('images/sandwitch.jpg'),
+                                  width: 28,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                'Sandwitch',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 28,
+                          ),
+                          Column(
+                            children: <Widget>[
+                              CircleAvatar(
+                                radius: 25,
+                                backgroundColor: Colors.white,
+                                child: Image(
+                                  image: AssetImage('images/icecream.jpg'),
+                                  width: 20,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                'IceCream',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 13,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                textColor: Colors.white,
-                onPressed: null,
               ),
-            )
-          ],
-        )),
+            ],
+          ),
+        ),
       ),
     );
   }
